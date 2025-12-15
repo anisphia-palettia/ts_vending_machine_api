49ed1bf40bc076cc94f0586d89c2505acf8cb8f8
Istianto Ilham <euphyllia.tomodachi@gmail.com>
2025-12-15 16:27:47 +0700

    Update seed.ts

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,7 +1,7 @@
 import { db } from "../../src/config/db.conf";
 
 async function seed() {
-  const hash = await Bun.password.hash("1234");
+  const hash = await Bun.password.hash("4321");
 
   await db.admin.upsert({
     where: { username: "1234" },
